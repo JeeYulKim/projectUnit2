dd8e95035b43ae2dbc7d40c621940150254ba0f3
JeeYulKim <86284203+JeeYulKim@users.noreply.github.com>
2023-02-14 15:16:13 +0900

    Add files via upload

---
+++ projectUnit2/src/test/java/com/practice/projectUnit2/ProjectUnit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.projectUnit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUnit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
